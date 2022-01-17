c24a18d3e41bcd6294a8065a39c58350e9ed5fd4
wardenlym <wardenlym@gmail.com>
2022-01-17 19:56:56 +0800

    fix

--- docs/istio-poc.md
+++
@@ -1,4 +0,0 @@
-# Isito
-
-Istio Gateway 资源本身只能配置L4到L6的功能，例如暴露的端口、TLS 设置等；但 Gateway 可与 VirtualService 绑定，在VirtualService 中可以配置七层路由规则，例如按比例和版本的流量路由，故障注入，HTTP 重定向，HTTP 重写等所有Mesh内部支持的路由规则。
-
--- src/golang-app-gateway/main.go
+++ src/golang-app-gateway/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/captcha", CaptchaServer)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Print(err)
@@ -73,4 +79,77 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Gateway get from upstream: [\n\n %s \n]\n", string(b))
+}
+
+const anticaptcha = "http://python-app-anticaptcha-svc:8080/anticaptcha/yk122"
+
+func CaptchaServer(w http.ResponseWriter, r *http.Request) {
+
+	req, err := newfileUploadRequest(anticaptcha, nil, "file", "math.jfif")
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v\n", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v\n", err)
+		return
+	}
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v\n", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(w, "Upstream error: %d\n", resp.StatusCode)
+		if resp.Body != nil {
+			b, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprintf(w, "Upstream error: Read body failed\n")
+				return
+			}
+			fmt.Fprintf(w, "Upstream error: %s\n", string(b))
+		}
+		return
+	}
+	if resp.Body == nil {
+		fmt.Fprintf(w, "Upstream error: No content\n")
+		return
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Upstream error: Read body failed\n")
+		return
+	}
+
+	fmt.Fprintf(w, "Gateway get from upstream: [\n\n AntiCaptcha: %s \n]\n", "")
+}
+
+// Creates a new file upload http request with optional extra params
+func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", uri, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, err
 }
